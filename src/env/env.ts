622054ad26c4c875b481c942ee808a0360b13f49
joaosp7 <joaosp7@gmail.com>
2025-01-19 16:20:22 -0300

    env.ts changes

---
+++ src/env/env.ts
@@ -0,0 +1,14 @@
+import z from "zod";
+
+const envSchema = z.object({
+  DB_PASSWORD: z.string(),
+});
+
+const envProcess = envSchema.safeParse(process.env);
+
+if (!envProcess.success) {
+  console.error(envProcess.error.format());
+  throw new Error("Invalid environment variables");
+}
+
+export const env = envProcess.data;
